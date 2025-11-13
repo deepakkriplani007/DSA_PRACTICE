37b1f3d50809c3a83bb692b8ed9dd251f3a7bdb3
deepak kriplani <122227033+deepakkriplani007@users.noreply.github.com>
2025-11-13 08:02:42 +0530

    Time: 4 ms (17.55%) | Memory: 9.8 MB (79.43%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,31 +1,11 @@
 class Solution {
 public:
-    bool alphabet(char c){
-        if(c<='Z'&& c>='A')return true;
-        if(c<='z'&& c>='a')return true;
-        if(c<='9'&& c>='0')return true;
-
-        return false;
-    }
-    bool alphanumeric(char c){
-        return false;
-        }
     bool isPalindrome(string s) {
-        string sn="";
-        for(int i=0;i<s.size();i++){
-            if(alphabet(s[i])){
-                sn+=tolower(s[i]);
-            }
-            
-        }
-        cout<<sn<<endl;
-        int i=0,j=sn.size()-1;
+        int i=0,j=s.size()-1;
         while(i<j){
-            if(sn[i]==sn[j]){
-                i++;j--;
-            }else{
-                return false;
-            }
+            while(i<j&&!isalnum(s[i])) i++;
+            while(i<j&&!isalnum(s[j])) j--;
+            if(tolower(s[i++])!=tolower(s[j--])) return false;
         }
         return true;
     }
