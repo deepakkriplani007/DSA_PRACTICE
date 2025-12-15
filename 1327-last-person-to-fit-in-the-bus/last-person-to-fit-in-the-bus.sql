ca08d5ec2144cd5ca5c443253e53bf24146e2b44
deepak kriplani <122227033+deepakkriplani007@users.noreply.github.com>
2025-12-15 08:58:16 +0530

    Time: 1022 ms (89.87%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -2,4 +2,4 @@ select top 1 person_name from
 (select turn,person_name,weight,sum(weight)over(order by turn)
  as nw from queue)
   t 
-  where t.nw<=1000 order by t.nw desc
+  where nw<=1000 order by nw desc
