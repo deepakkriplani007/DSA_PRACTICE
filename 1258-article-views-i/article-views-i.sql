d963946984d8b97b86e8a9d7fd093285f6224725
deepak kriplani <122227033+deepakkriplani007@users.noreply.github.com>
2026-01-26 18:31:10 +0530

    Time: 767 ms (76.39%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,2 +1,2 @@
 /* Write your T-SQL query statement below */
-select author_id as id from views where viewer_id=author_id group by author_id
+select distinct author_id as id from views where viewer_id=author_id
