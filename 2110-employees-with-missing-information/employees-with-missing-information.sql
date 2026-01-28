9b4de9e4e9785772f2325a92b4c292df61b80676
deepak kriplani <122227033+deepakkriplani007@users.noreply.github.com>
2026-01-28 11:26:56 +0530

    Time: 1230 ms (5.11%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,7 +1,7 @@
 /* Write your T-SQL query statement below */
-select employee_id from
-(select case when e.name is null or  s.salary is null then
-(case when s.employee_id is null then e.employee_id when e.employee_id is null then s.employee_id end) 
-end as employee_id
-from employees e full outer join salaries s on e.employee_id=s.employee_id)
-a where employee_id is not null order by employee_id
+SELECT 
+    COALESCE(s.employee_id, e.employee_id ) employee_id 
+FROM Employees e
+FULL OUTER JOIN Salaries s on e.employee_id = s.employee_id
+WHERE e.employee_id IS NULL OR s.employee_id IS NULL
+ORDER BY employee_id
