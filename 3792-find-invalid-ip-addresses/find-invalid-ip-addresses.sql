f9985755292ad10eb6d7198ce80c884cc9ca94ed
deepak kriplani <122227033+deepakkriplani007@users.noreply.github.com>
2026-02-04 13:37:53 +0530

    Time: 276 ms (50.56%) | Memory: 0B (100.00%) - LeetSync

--- 3792-find-invalid-ip-addresses/find-invalid-ip-addresses.sql
+++ 3792-find-invalid-ip-addresses/find-invalid-ip-addresses.sql
@@ -1,28 +1,13 @@
-WITH distinct_ips AS (
-    SELECT DISTINCT ip
-    FROM logs
-),
-split_ip AS (
-    SELECT
-        d.ip,
-        value AS octet,
-        COUNT(*) OVER (PARTITION BY d.ip) AS octet_count
-    FROM distinct_ips d
-    CROSS APPLY STRING_SPLIT(d.ip, '.')
-),
-invalid_ips AS (
-    SELECT DISTINCT ip
-    FROM split_ip
-    WHERE
-        octet_count <> 4
-        OR TRY_CAST(octet AS INT) > 255
-        OR (LEN(octet) > 1 AND octet LIKE '0%')
-)
-SELECT
-    l.ip,
-    COUNT(*) AS invalid_count
-FROM logs l
-JOIN invalid_ips i
-    ON l.ip = i.ip
-GROUP BY l.ip
-ORDER BY invalid_count DESC, l.ip DESC;
+SELECT L.ip,
+COUNT(L.ip)
+AS invalid_count
+FROM logs L
+GROUP BY L.ip
+HAVING (
+    CASE WHEN (SELECT COUNT(*) FROM STRING_SPLIT(L.ip,'.')) <> 4 THEN 1 ELSE 0 END 
+    +
+    CASE WHEN (SELECT COUNT(*) FROM STRING_SPLIT(L.ip,'.') WHERE CAST([VALUE] AS INT) > 255 ) > 0 THEN 1 ELSE 0 END 
+    +
+    CASE WHEN (SELECT COUNT(*) FROM STRING_SPLIT(L.ip,'.') WHERE [VALUE] LIKE '0%') > 0 THEN 1 ELSE 0 END ) > 0
+
+ORDER BY COUNT(L.ip) DESC, L.ip DESC
