b7a6aa0da8baeefedefced929b513bc95f47ef14
deepak kriplani <122227033+deepakkriplani007@users.noreply.github.com>
2026-02-12 12:03:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (99.11%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
- int findPascalElement(int r, int c) {
-        // Element is C(r-1, c-1)
-        int n = r - 1;
-        int k = c - 1;
-
-        long long result = 1;
-
-        // Compute C(n, k) using iterative formula
-        for (int i = 0; i < k; i++) {
-            result *= (n - i);
-            result /= (i + 1);
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> pascal;
+        for(int i = 0; i < numRows; i++){
+            vector<int> row(i+1, 1);
+            for(int j = 1; j < i; j++){
+                row[j] = pascal[i-1][j-1] + pascal[i-1][j];
+            }
+            pascal.push_back(row);
         }
-
-        return result;
-    }
-    vector<vector<int>> generate(int nr) {
-      vector<vector<int>>ans;
-      for(int i=1;i<=nr;i++){
-            vector<int>p;
-        for(int j=1;j<=i;j++){
-            p.push_back(findPascalElement(i,j));
-        }
-        ans.push_back(p);
-      } 
-      return ans;
+        return pascal;
+        
     }
 };
